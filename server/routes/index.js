6937e5e0fbe2ae34861d855c5fc0cef33e5f4d55
Ahmad <ahmadsermani58@gmail.com>
2020-04-03 21:18:22 +0300

    set up

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 function routes(app) {
-  router.get("/movies", (req, res) => {
-    res.end("We made it! And it's great");
-  });
-  
-  router.get("/movies/:id", (req, res) => {
-    return app.render(req, res, "/movies", { id: req.params.id });
-  });
 
-  return router;
+    app.get('/api', (req, res) => {
+        res.json({ Name: 'ahmad sermani' })
+    })
+
+
+    return router;
 };
 
 module.exports = routes;
